0c07bd0b2c3bd0c3c263ddb33a272dc2af0d6771
Daniel Quintero <dgquintero02@hotmail.com>
2024-02-20 15:19:24 -0500

    adding test add function

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from simple_calculator import SimpleCalculator
+
+def test_add_two_numbers():
+    calculator = SimpleCalculator()
+    result = calculator.add(5, 4)
+    assert result == 9
+
 
