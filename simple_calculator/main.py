60c4f78de427def88d1311c913a4c95c405f7ff3
Daniel Quintero <dgquintero02@hotmail.com>
2024-02-21 11:04:41 -0500

    substraction

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+
+class SimpleCalculator:
+    """ SimpleCalculator class"""
+    def add(self, *args):
+        """ add method"""
+        return sum(args) # sum function returns the sum of all the elements in a list
+
+    def substract(self, a, b):
+        """ substract method"""
+        return a - b
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from simple_calculator import SimpleCalculator
+from simple_calculator.main import SimpleCalculator # simplecalculator class is
+# imported from simple_calculator.py
 
 def test_add_two_numbers():
-    calculator = SimpleCalculator()
+    """ test add_two_numbers"""
+    calculator = SimpleCalculator() # instatiated the class
     result = calculator.add(5, 4)
-    assert result == 9
+    assert result == 9 # first computes result == 9 -> boolean assert passed if is True otherwise raise exception if result is False
 
+def test_add_three_numbers():
+    """ test add_three_numbers"""
+    calculator = SimpleCalculator() # instatiated the class calculator
+
+    result = calculator.add(4, 5, 6)
+
+    assert result == 15# first computes result == 15 -> boolean assert passed if is True otherwise raise exception if result is False
+
+def test_add_multiple_numbers():
+    """ test add_multiple_numbers"""
+    calculator = SimpleCalculator() # instatiated the class calculator
+
+    result = calculator.add(1, 2, 3, 4, 5)
+
+    assert result == 15 # first computes result == 15 -> boolean assert passed if is True otherwise raise exception if result is False
+
+def test_substact_two_numbers():
+    """ test substact_two_numbers"""
+    calculator = SimpleCalculator() # instatiated the class calculator
+
+    result = calculator.substract(5, 4)
+
+    assert result == 1 # first computes result == 1 -> boolean assert passed if is True otherwise raise exception if result is False
 
